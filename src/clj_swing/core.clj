b6ee4b50ab3d556725acf5d38acc75751934589e
Heinz N. Gies <heinz@licenser.net>
2010-05-01 20:48:50 +0300

    Added utility functions.

--- src/clj_swing/core.clj
+++ src/clj_swing/core.clj
@@ -10,21 +10,29 @@
   (reduce dissoc m ks))
 
 (defn icon-setters [names opts]
-  (remove 
-   nil?
-   (map
-    (fn [name] 
-      (when-let [icon (opts name)] 
-	`(.  ~(kw-to-setter name) (.getImage (ImageIcon. ~icon))))) names)))
+  (if opts
+    (remove 
+     nil?
+     (map
+      (fn [name] 
+	(when-let [icon (opts name)] 
+	  `(.  ~(kw-to-setter name) (.getImage (ImageIcon. ~icon))))) names))))
 
 (defn auto-setters [cl known-kws opts]
-  (map (fn [[a v]] (list 
-		    '. 
-		    (kw-to-setter a) 
-		    (if (keyword? v)
-		      `(. ~cl ~(symbol (st/upper-case (name v))))
-		      v)))
-       (remove-known-keys opts known-kws)))
+  (if opts
+    (map (fn [[a v]] (list 
+		      '. 
+		      (kw-to-setter a) 
+		      (if (keyword? v)
+			`(. ~cl ~(symbol (st/upper-case (name v))))
+			v)))
+	 (remove-known-keys opts known-kws))))
+
+(defn insert-at [seq idx item]
+  (concat (take idx seq) [item] (drop idx seq)))
+
+(defn drop-nth [seq idx]
+  (concat (take idx seq) (drop (inc idx) seq)))
 
 (defmacro add-action-listener [obj [[event] & code]]
   `(doto ~obj
@@ -33,25 +41,6 @@
 	(actionPerformed [~event]
 			 ~@code)))))
 (comment
-(import '(javax.swing JFrame JLabel JTextField JButton JComboBox JPanel Timer)
-	'(java.awt.event ActionListener)
-	'(java.awt GridBagLayout GridLayout GridBagConstraints))
-
-
-
-(defmacro set-text! [component text]
-  `(. ~component setText ~text))
-
-
-
-
-(defn selected-item [obj]
-  (.getSelectedItem obj))
-
-(defmacro combo-box [[& items] & actions]
-  `(doto (JComboBox.)
-     ~@(map #(list '.addItem %) items)
-     ~@actions))
 
 (defmacro panel [& args]
   `(JPanel. ~@args))
