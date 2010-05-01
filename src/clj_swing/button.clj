4871c750b970598497b9af4d6dd4e34f8293c11c
Heinz N. Gies <heinz@licenser.net>
2010-05-01 17:26:15 +0300

    Added remaining buttons and button group.

--- src/clj_swing/button.clj
+++ src/clj_swing/button.clj
@@ -1,21 +1,44 @@
 (ns clj-swing.button
   (:use [clj-swing.core :only [add-action-listener icon-setters auto-setters]])
-  (:import (javax.swing JButton ImageIcon)))
+  (:import (javax.swing JButton JCheckBox ImageIcon JCheckBoxMenuItem JRadioButton JRadioButtonMenuItem JToggleButton ButtonGroup)))
 
 
 (def *button-icon-keys* 
      [:icon :disabled-icon :selected-icon :pressed-icon :disabled-selected-icon :rollover-icon :rollover-selected-icon ])
 (def *button-known-keys*
-     (concat [:action] *button-icon-keys*))
-   
+     (concat [:action :caption] *button-icon-keys*))
 
-(defmacro button [title & {action :action :as opts}]
-  (let [b (or name (gensym "btn"))]
-    `(let [~b  (JButton.)]
+
+(defmacro general-button [cl {caption :caption action :action :as opts}]
+  (let [b (gensym "btn")]
+    `(let [~b  (new ~cl)]
        (doto ~b
-	 ~@(if title  
-	     [`(.setText ~title)])
+	 ~@(if caption  
+	     [`(.setText ~caption)])
 	 ~@(if action  
 	     [`(add-action-listener ~action)])
 	 ~@(icon-setters *button-icon-keys*  opts)
 	 ~@(auto-setters JButton *button-known-keys* opts)))))
+
+(defmacro button [caption & {:as opts}]
+  `(general-button JButton ~(assoc opts :caption caption)))
+
+(defmacro check-box [ & {:as opts}]
+  `(general-button JCheckBox ~opts))
+
+(defmacro check-box-menu-item [ & {:as opts}]
+  `(general-button JCheckBoxMenuItem ~opts))
+
+(defmacro radio-button [ & {:as opts}]
+  `(general-button JRadioButton ~opts))
+
+(defmacro radio-button-menu-item [ & {:as opts}]
+  `(general-button JRadioButtonMenuItem ~opts))
+
+(defmacro toggle-button [ & {:as opts}]
+  `(general-button JToggleButton ~opts))
+
+(defmacro button-group [& buttons]
+  `(doto (ButtonGroup.)
+     ~@(map (fn [btn]
+	      `(.add ~btn)) buttons)))
